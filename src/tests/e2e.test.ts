b0059e2fa6eaacbf27b75682ee69aada52396c2d
suhaotian <h4av.one@gmail.com>
2025-02-25 21:40:46 +1100

    Update

--- src/tests/e2e.test.ts
+++ src/tests/e2e.test.ts
@@ -1,3 +1,4 @@
+import fss from 'fs';
 import fs from 'fs/promises';
 import test, { describe } from 'node:test';
 
@@ -6,15 +7,18 @@ import { run } from '..';
 describe('e2e tests', () => {
   test('parseConditions should handle case insensitivity for units', async () => {
     await fs.writeFile('./.gitattributes', '');
-    const files: string[] = [
-      'imgs/dive.jpg',
-      'imgs/dive3.jpg',
-      'package.json',
-      // 'small.txt',
-      // 'large.txt',
-      // 'small.jpg',
-      // 'large.jpg',
-    ];
+    const isSmallTxtExists = fss.existsSync('small.txt');
+    const files: string[] = isSmallTxtExists
+      ? [
+          'imgs/dive.jpg',
+          'imgs/dive3.jpg',
+          'package.json',
+          'small.txt',
+          'large.txt',
+          'small.jpg',
+          'large.jpg',
+        ]
+      : ['imgs/dive.jpg', 'imgs/dive3.jpg', 'package.json'];
     const result = await run('image,video,audio:100kb;*:1024kb', files);
     console.log('run() result: ');
     console.log(result);
